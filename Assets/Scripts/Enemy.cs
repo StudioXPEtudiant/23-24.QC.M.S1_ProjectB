b85ee91480827420241379e2df06ea3464d92bc4
Rissoy <rsth2208@gmail.com>
2023-11-28 18:23:54 -0500

    COMMIT bg de la street

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -57,7 +57,7 @@ public class Enemy : MonoBehaviour {
                         StartCoroutine(Cooldown());
                     }
 
-                    FaceTarget();
+                    //FaceTarget();
                 }
             }
             else
