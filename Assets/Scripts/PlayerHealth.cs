8dfca2f345dc749f2cc9746c1384c4e614098b10
Rissoy <rsth2208@gmail.com>
2023-12-05 19:21:13 -0500

    AJOUT D'UN TIMER YAY

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,11 @@ public class PlayerHealth : MonoBehaviour
 {
     public int health = 3;
     public Slider healthBar;
+
+    private void Start()
+    {
+        healthBar.maxValue = health;
+    }
 
     // Update is called once per frame
     void Update()
