b3e53f9b2ea0d22190dcb9ec1e9c0554a4c37950
Rissoy <rsth2208@gmail.com>
2024-01-23 18:27:15 -0500

    Prototype v01

--- Assets/Scripts/PlayerMovementRyan.cs
+++ Assets/Scripts/PlayerMovementRyan.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class PlayerMovementRyan : MonoBehaviour
 {
     public CharacterController controller;
 
